388abac3215db33db84e49c401dd312aaf4f26f4
Juan David <juandtrujillo99@gmail.com>
2022-07-20 22:04:56 +0200

    cambio aplicado a la vista de las entradas en el blog

--- pagina/blog/entrada.php
+++ pagina/blog/entrada.php
@@ -54,11 +54,10 @@ include_once 'seccion/doc-navbar.inc.php';
             <div class="sharethis-inline-share-buttons"></div>
         </div>
         <?php //para moviles ?>
-        <div class="col-12 d-block d-sm-none" style="overflow-y: scroll;padding: 3em 4em; scroll-behavior: smooth;">
-            <h1 class="textoBlack"><?php echo $entrada -> obtener_titulo(); ?></h1>
+        <div class="col-12 d-block d-sm-none" style="overflow-y: scroll;padding: 2.8em; scroll-behavior: smooth;">
+            <p style="font-size: 1.5em"><?php echo nl2br($entrada -> obtener_texto()); ?></p>
+            <br>
             <div class="textoParrafo1a">Publicado el <?php echo convertirFecha($entrada -> obtener_fecha()); ?> por <?php echo $autor -> obtener_nombre(); ?></div>
-            <br><br>
-            <p align="justify"><?php echo nl2br($entrada -> obtener_texto()); ?></p>      
             <br><br>
             <div class="sharethis-inline-share-buttons"></div>
             <br><br>
--- seccion/blog/entradas_al_azar.inc.php
+++ seccion/blog/entradas_al_azar.inc.php
@@ -7,12 +7,11 @@ include_once 'app/blog/EscritorEntradas.inc.php';
 		for ($i = 0; $i < count($entradas_azar); $i++) {
 			$entrada_actual = $entradas_azar[$i];
 		?>		
-		<a href="<?php echo $entrada_actual -> obtener_url(); ?>" class="col-md-3 col-6">
-            <div class="row card" style="margin: .5em;">
+		<a href="<?php echo $entrada_actual -> obtener_url(); ?>" class="col-md-3 col-12">
+            <div class="row card" style="margin: .5em;background-color: none; border: none;">
 				<div class="card-image">					
 		            <div align="center">   
-		                <div class="img-entrada bg-cover d-none d-sm-block" style="background-image:url(<?php echo RUTA_BLOG_COVER.$entrada_actual -> obtener_imagen(); ?>);"></div>
-		                <div class="img-entrada-m bg-cover d-block d-sm-none" style="background-image:url(<?php echo RUTA_BLOG_COVER.$entrada_actual -> obtener_imagen(); ?>);"></div>
+		                <div class="img-entrada bg-cover" style="background-image:url(<?php echo RUTA_BLOG_COVER.$entrada_actual -> obtener_imagen(); ?>);"></div>
 		            </div>
 		        </div>        
             </div>
