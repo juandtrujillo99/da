d100d47b4f108a5f73307bd5cae8f6391f52d2c2
Juan David <juandtrujillo99@gmail.com>
2022-07-22 06:28:57 +0200

    corregido el error de la subida de imagenes al servidor

--- scripts/blog/barra-progreso-archivo-imagen.php
+++ scripts/blog/barra-progreso-archivo-imagen.php
@@ -1,36 +1,36 @@
 <script async='async'>
     function _(el){
         return document.getElementById(el);
-    } 
+    }    
 
-    function subirImagen(){//sube las fotos de portada de las imagenes
-        var file = _("file1").files[0];
+    function subirImagenes(){//sube las fotos de portada de las imagenes
+        var file = _("archivoImagen").files[0];
         //alert(file.name+" | "+file.size+" | "+file.type);
         var formdata = new FormData();
-        formdata.append("file1", file);
+        formdata.append("archivoImagen", file);
         var ajax = new XMLHttpRequest();
         ajax.upload.addEventListener("progress", progressHandler, false);
         ajax.addEventListener("load", completeHandler, false);
         ajax.addEventListener("error", errorHandler, false);
         ajax.addEventListener("abort", abortHandler, false);
-        ajax.open("POST", "<?php echo 'app/blog/CargarArchivo.inc.php';?>");
+        ajax.open("POST", "<?php echo 'app/blog/CargarArchivoImagen.inc.php';?>");
         ajax.send(formdata);
 
         function progressHandler(event){
         //_("loaded_n_total").innerHTML = event.loaded+" bytes subidos de "+event.total;
         var percent = (event.loaded / event.total) * 100;
-        _("progressBar").value = Math.round(percent);
-        _("status").innerHTML = Math.round(percent)+"% subido... Por favor espera.";
+        _("progressBarImagen").value = Math.round(percent);
+        _("statusFile").innerHTML = Math.round(percent)+"% subido... Por favor espera.";
         }
         function completeHandler(event){
-            _("status").innerHTML = event.target.responseText;
-            _("progressBar").value = 0;
+            _("statusFile").innerHTML = event.target.responseText;
+            _("progressBarImagen").value = 0;
         }
         function errorHandler(event){
-            _("status").innerHTML = "Falló la subida";
+            _("statusFile").innerHTML = "Falló la subida";
         }
         function abortHandler(event){
-            _("status").innerHTML = "Subida cancelada";
+            _("statusFile").innerHTML = "Subida cancelada";
         }
     }
 
